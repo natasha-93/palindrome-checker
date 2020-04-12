cc63d083ce97c4423b0842ef86d91c27502d8cb0
Natasha Lawson <natasha.lawson93@gmail.com>
2020-04-12 15:54:58 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+function palindrome(string) {
+  const re = /[^A-Za-z0-9]/g;
+  const lowerCase = string.toLowerCase().replace(re, "");
+  const reverseString = lowerCase.split("").reverse("").join("");
+  return string === reverseString;
+}
 
 function App() {
+  const [word, setWord] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={word}
+        onChange={(e) => {
+          setWord(e.target.value);
+        }}
+      />
+      {word.length > 0 && (
+        <p>{palindrome(word) ? "is a palindrome!" : "is not a palindrome!"}</p>
+      )}
     </div>
   );
 }
